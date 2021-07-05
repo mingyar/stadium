d1a91384adef276d81cd4aac54d489f44a432234
Mingyar Furtado <mingyar.furtado@gmail.com>
2021-07-05 04:50:01 -0300

    Adding more documentation

--- lib/stadium_web/controllers/api/battle_controller.ex
+++ lib/stadium_web/controllers/api/battle_controller.ex
@@ -3,6 +3,28 @@ defmodule StadiumWeb.BattleController do
   alias Stadium.Battles
   alias Stadium
 
+  @doc """
+  Ask Stadium to set up a new battle, by *given two pokemon ids*,
+  store the battle and render the new battle created.
+
+  ## Parameters
+
+    - *pokemon_id_1*: First Pokemon id on https://pokeapi.co/api/v2/pokemon/
+    - *pokemon_id_2*: Second Pokemon id on https://pokeapi.co/api/v2/pokemon/
+
+  ## Example
+    By hitting a POST against:
+    http://localhost:4000/api/battle/create?pokemon_id_1=8&pokemon_id_2=10
+
+    It will return a JSON as follows:
+    ```
+    {
+      "oppenents":"wartortle X caterpie",
+      "the_winner_is":"wartortle",
+      "url":"http://localhost:4000/api/battle/42"
+    }
+    ```
+  """
   def create(conn, %{"pokemon_id_1" => pokemon_id_1, "pokemon_id_2" => pokemon_id_2}) do
     battle = Stadium.set_up_battle(pokemon_id_1, pokemon_id_2)
     with {:ok, battle} <- Battles.create_battle(battle) do
@@ -11,7 +33,8 @@ defmodule StadiumWeb.BattleController do
   end
 
   @doc """
-  Ask Stadium to set up a new battle, store the battle and render the new battle created.
+  Creates a new Battle *with random pokemons*,
+  store the battle and render the new battle created.
   """
   def create(conn, _params) do
     battle = Stadium.set_up_battle
@@ -22,6 +45,22 @@ defmodule StadiumWeb.BattleController do
 
   @doc """
   Ask Battle repository to get the chosen battle by given id and render it.
+
+  ## Example
+    By hitting a GET against:
+    http://localhost:4000/api/battle/12
+
+    It will return a JSON as follows:
+    ```
+    {
+      "battle": {
+        "id": 12,
+        "pokemon_1": "shroomish",
+        "pokemon_2": "urshifu-single-strike",
+        "winner": "shroomish"
+      }
+    }
+    ```
   """
   def show(conn, %{"id" => battle_id}) do
     battle = Battles.get_battle(battle_id)
@@ -30,6 +69,36 @@ defmodule StadiumWeb.BattleController do
 
   @doc """
   Ask Battle repository to get all battles to render them.
+
+  ## Example
+    By hitting a GET against:
+    http://localhost:4000/api/battles
+
+    It will return a JSON as follows:
+    ```
+    {
+    "battles": [
+        {
+            "battle": {
+                "id": 4,
+                "pokemon_1": "kabutops",
+                "pokemon_2": "deoxys-normal",
+                "winner": "kabutops"
+            },
+            "url": "http://localhost:4000/api/battle/4"
+        },
+        {
+            "battle": {
+                "id": 5,
+                "pokemon_1": "tyrantrum",
+                "pokemon_2": "swablu",
+                "winner": "tyrantrum"
+            },
+            "url": "http://localhost:4000/api/battle/5"
+        }
+      ]
+    }
+    ```
   """
   def index(conn, _params) do
     all_battles = Battles.get_all
