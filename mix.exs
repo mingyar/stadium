e9d83cde938a64e6b80cf9fdb5bc7a9df9f001f6
Mingyar Furtado <mingyar.furtado@gmail.com>
2021-07-04 22:33:22 -0300

    Installing Credo

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule Stadium.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:httpoison, "~> 1.8"},
-      {:poison, "~> 3.1"}
+      {:poison, "~> 3.1"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
     ]
   end
 
